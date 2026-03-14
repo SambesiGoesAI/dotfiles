8509b8f964e7d81b9605df4444ce51c5f43d4875
SambesiGoesAI <sami@andsome.fi>
2026-03-14 21:18:10 +0000

    Smart install - clone or pull

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 DOTFILES="$HOME/dotfiles"
 
-echo "🔧 Installing dotfiles..."
+echo "🔧 Updating dotfiles..."
 
+if [ -d "$DOTFILES/.git" ]; then
+  cd $DOTFILES && git pull
+else
+  git clone https://SambesiGoesAI:TOKEN@github.com/SambesiGoesAI/dotfiles.git $DOTFILES
+fi
+
+mkdir -p ~/.config
 ln -sf $DOTFILES/tmux/.tmux.conf ~/.tmux.conf
 ln -sf $DOTFILES/starship/.config/starship.toml ~/.config/starship.toml
 ln -sf $DOTFILES/zsh/.zshrc ~/.zshrc
